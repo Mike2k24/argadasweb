d560ca5c9c0e740094ef9e3f2c7fc1dd80346ef6
Mike2k24 <166922579+Mike2k24@users.noreply.github.com>
2024-12-26 17:37:21 +0000

    Image fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ export default function Home(){
 
     return(
         <div >
-          <img className='the-boys' src="../public/images/jason-center.png" alt="ARGADAS Image here" />
+          <img className='the-boys' src="./images/jason-center.png" alt="ARGADAS Image here" />
       </div>
     )
 }
